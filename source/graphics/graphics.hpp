b05cfb9b02e97328bcaa978b9450cf0a66cb3774
David <huangd95@gmail.com>
2020-06-06 14:01:31 -0700

    Adding graphic types

---
+++ source/graphics/graphics.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <cstdint>
+
+namespace graphics
+{
+struct Color_t
+{
+  uint8_t red;
+  uint8_t green;
+  uint8_t blue;
+};
+
+static constexpr Color_t kWhite =
+    Color_t{ .red = 0xFF, .green = 0xFF, .blue = 0xFF };
+static constexpr Color_t kBlack =
+    Color_t{ .red = 0x00, .green = 0x00, .blue = 0x00 };
+static constexpr Color_t kRed =
+    Color_t{ .red = 0xFF, .green = 0x00, .blue = 0x00 };
+static constexpr Color_t kGreen =
+    Color_t{ .red = 0x00, .green = 0xFF, .blue = 0x00 };
+static constexpr Color_t kBlue =
+    Color_t{ .red = 0x00, .green = 0x00, .blue = 0xFF };
+
+struct Point_t
+{
+  uint16_t x;
+  uint16_t y;
+};
+
+struct Size_t
+{
+  uint16_t width;
+  uint16_t height;
+};
+
+struct Frame_t
+{
+  Point_t origin;
+  Size_t size;
+
+  explicit Frame_t(uint16_t x, uint16_t y, uint16_t width, uint16_t height)
+      : origin({ .x = x, .y = y }), size({ .width = width, .height = height })
+  {
+  }
+};
+}  // namespace graphics
