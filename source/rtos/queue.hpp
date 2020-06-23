4ced3944ac8f92117098f3d0c446e320a9c2964e
David <huangd95@gmail.com>
2020-06-23 15:46:25 -0700

    Renamed member variables in Queue wrapper

--- source/rtos/queue.hpp
+++ source/rtos/queue.hpp
@@ -15,38 +15,33 @@ class Queue
  public:
   Queue()
   {
-    queue_handle_ = xQueueCreateStatic(
-        kQueueSize, sizeof(T), queue_storage_area_, &static_queue_buffer_);
-  }
-
-  QueueHandle_t GetHandle()
-  {
-    return queue_handle_;
+    handle_ = xQueueCreateStatic(
+        kQueueSize, sizeof(T), storage_area_, &buffer_);
   }
 
   BaseType_t Send(T * item, TickType_t timeout)
   {
-    return xQueueSend(queue_handle_, item, timeout);
+    return xQueueSend(handle_, item, timeout);
   }
 
   BaseType_t SendToFront(T * item, TickType_t timeout)
   {
-    return xQueueSendToFront(queue_handle_, item, timeout);
+    return xQueueSendToFront(handle_, item, timeout);
   }
 
   BaseType_t Receive(void * location, TickType_t timeout)
   {
-    return xQueueReceive(queue_handle_, location, timeout);
+    return xQueueReceive(handle_, location, timeout);
   }
 
   BaseType_t Reset()
   {
-    return xQueueReset(queue_handle_);
+    return xQueueReset(handle_);
   }
 
  private:
-  uint8_t queue_storage_area_[kQueueSize * sizeof(T)];
-  StaticQueue_t static_queue_buffer_;
-  QueueHandle_t queue_handle_;
+  uint8_t storage_area_[kQueueSize * sizeof(T)];
+  StaticQueue_t buffer_;
+  QueueHandle_t handle_;
 };
 }  // namespace rtos
