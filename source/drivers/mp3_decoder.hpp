9e25da5dc8a231350c30438be8ff2b9b5dd9d279
David <huangd95@gmail.com>
2020-06-23 21:44:05 -0700

    Moving Mp3File_t to its own header file and into the mp3 namespace

--- source/drivers/mp3_decoder.hpp
+++ source/drivers/mp3_decoder.hpp
@@ -1,35 +1,6 @@
 #pragma once
 
-#include <cstddef>
 #include <cstdint>
-
-struct Mp3File_t
-{
-  /// File path.
-  const char * path;
-  /// Size of the file in bytes.
-  size_t size;
-  /// ID3v1 metadata of a MP3 file.
-  ///
-  /// @see https://en.wikipedia.org/wiki/ID3
-  struct
-  {
-    ///  3 bytes - should contain "TAG"
-    char header[3];
-    /// 30 bytes - 30 characters of title
-    char title[30];
-    /// 30 bytes - 30 characters of artist name
-    char artist[30];
-    /// 30 bytes - 30 characters of album name
-    char album[30];
-    ///  4 bytes - 4 digit year
-    uint32_t year;
-    /// 30 bytes - Comment
-    char comment[30];
-    ///  1 byte  - Index of the track's genre (0-255)
-    uint8_t genre;
-  } metadata = {};
-};
 
 /// An interface containing controls for a MP3 audio decoder.
 class Mp3Decoder
